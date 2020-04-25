e175c6da3c68a2242b04958ee5bec7d81f4ddb95
jefersonrjrn <jeferson.rjrn@gmail.com>
2020-04-25 16:33:04 -0300

    AddEventListener para mudar forma se pegar o input

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
+const topright = document.querySelector('#topright');
+const topleft = document.querySelector('#topleft');
+const botright = document.querySelector('#botright');
+const botleft = document.querySelector('#botleft');
 
+topright.addEventListener("input", (event) => {
+  let border = topright.value;
 
-function borderChange() {
-  var box = document.querySelector('#box');
-  var border = document.querySelector('input').value;
+  document.getElementById('box').style.borderTopRightRadius = border + "px";
+});
 
-  document.getElementById('box').style.borderRadius = border + "px";
-}
+topleft.addEventListener("input", (event) => {
+  let border = topleft.value;
+
+  document.getElementById('box').style.borderTopLeftRadius = border + "px";
+});
+
+botright.addEventListener("input", (event) => {
+  let border = botright.value;
+
+  document.getElementById('box').style.borderBottomRightRadius = border + "px";
+});
+
+botleft.addEventListener("input", (event) => {
+  let border = botleft.value;
+
+  document.getElementById('box').style.borderBottomLeftRadius = border + "px";
+});
+
 
